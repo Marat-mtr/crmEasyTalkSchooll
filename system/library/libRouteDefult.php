8a883d8b60f9462431d1a190dbb1f1d5d3cc9b19
Marat-mtr <ruslandhost@gmail.com>
2023-10-07 22:30:31 +0300

    v1

--- system/library/libRouteDefult.php
+++ system/library/libRouteDefult.php
@@ -1,9 +1,30 @@
 <?php
 
-$routes = [
-    'crm' => ['controller' => 'index'] 
+$routeAdmin = [
+    'admin' => [
+        'home' => 'admin/mainAdmin',
+        'login' => 'admin/login'
+    ]
 ];
 
+
+
+$routeCommon = [
+    'common' => [
+        'home' => 'common/home',
+        'userslog' => 'common/users/login',
+        'usersreg' => 'common/users/userRegist',
+        'aboutUs'  => 'common/aboutUs',
+        'noFound'  => 'common/noFound'
+    ],
+];
+
+
+
+$routeCrm = [
+    'crm' => ['controller' => 'index'],
+
+];
 
 
 
